76a1aa09bec9e6317298f8bdf8101394b55d053f
Mahib Abrar <112199842+Coding-Mahib@users.noreply.github.com>
2023-04-15 22:04:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const wizard = document.querySelector('#wizard');
 const pl = document.querySelector('#pl');
 const pl_c = document.querySelector('#pl-c');
-const api = 'http://trainojatri.freewebhosting.com.bd/index.php';
+const api = 'https://trainojatri-api.000webhostapp.com/api/api.php';
 const access_token = '151090987';
